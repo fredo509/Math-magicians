0f7f988a43e038049cf753ec9f91e48ebd6a7c5e
Fredo St Fleur <fredostfleur23@gmail.com>
2023-04-24 14:34:20 -0400

    - Add react config -Add git ignore

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Update comming ...
+        </p>
+        <a
+          className="App-link"
+          href="https://clipground.com/images/math-genius-clipart-3.png"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MATH MAGICIANS !!! 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
