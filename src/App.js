7b779f7b08f07d763538661715825e192cf909ce
Fredo St Fleur <fredostfleur23@gmail.com>
2023-04-26 13:08:15 -0400

    -Import new components -Send html template to be rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import theCalculator from './components/calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (theCalculator());
 }
 
 export default App;
