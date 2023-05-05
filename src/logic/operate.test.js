8e71905d652dee24bbe56d2ffe71bbe08ebcc9a1
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-05-05 18:45:58 -0300

    Add operate.js tests

---
+++ src/logic/operate.test.js
@@ -0,0 +1,28 @@
+import operate from './operate';
+
+describe('operate function', () => {
+  test('adds two numbers', () => {
+    expect(operate(1, 2, '+')).toBe('3');
+  });
+
+  test('subtracts two numbers', () => {
+    expect(operate(5, 3, '-')).toBe('2');
+  });
+
+  test('multiplies two numbers', () => {
+    expect(operate(4, 3, 'x')).toBe('12');
+  });
+
+  test('divides two numbers', () => {
+    expect(operate(6, 2, '÷')).toBe('3');
+  });
+
+  test('finds the modulo of two numbers', () => {
+    expect(operate(7, 4, '%')).toBe('3');
+  });
+
+  test('throws an error when the operation is unknown', () => {
+    expect(() => operate(1, 2, '&')).toThrow("Unknown operation '&'");
+  });
+
+});
