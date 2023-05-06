778218f4e3973deecbab7d433f9c9e2c0e114c66
Fredo St Fleur <fredostfleur23@gmail.com>
2023-05-06 13:41:49 -0400

    Fix path as required

--- src/tests/navBar.test.js
+++ src/tests/navBar.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// eslint-disable-next-line
-import NavBar from '../components/Navbar';
+import NavBar from '../components/navBar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
